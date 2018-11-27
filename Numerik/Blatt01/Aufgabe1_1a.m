2bfb862694a2b7aa7752d425d29233fcd177c557
Syhon <lars.kolk@tu-dortmund.de>
2018-11-27 23:42:37 +0100

    KET, war aber zu faul zum sortieren, mache ich später

---
+++ Numerik/Blatt01/Aufgabe1_1a.m
@@ -0,0 +1,23 @@
+%aaaa
+format long
+function euler(x,n)
+  k=0;
+  sum=0;
+while k <= n
+  sum+=(x^k)/factorial(k);
+  k+=1;
+end
+printf("Ergebnis für n =");
+disp(n);
+printf("  ");
+disp(sum);
+printf("\n")
+endfunction 
+
+printf("1.1 a) : ")
+printf("\n")
+i=3
+while i <= 30
+  euler(-5.5, i);
+  i+=3;
+end 
